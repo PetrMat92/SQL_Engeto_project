42a566048a98e4e7a6a5484ffb876f93f0e11a10
Petr Matějíček <114687356+PetrMat92@users.noreply.github.com>
2023-04-18 07:44:23 +0200

    Create t_{petr}_{matějíček}_project_SQL_primary_final.sql

---
+++ t_{petr}_{matějíček}_project_SQL_primary_final.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS t_petr_matejicek_project_SQL_secondary_final AS (
+	SELECT
+		c.country,
+		e.`year`, 
+		e.GDP 
+	FROM countries c 
+	JOIN economies e 
+		ON c.country = e.country
+		WHERE e.`year`BETWEEN (SELECT MIN(`year`)  
+		FROM t_petr_matejicek_project_SQL_primary_final
+		WHERE data_type = 'Průměrná cena za jednotku') AND (SELECT MAX(`year`)
+		FROM t_petr_matejicek_project_SQL_primary_final
+		WHERE data_type = 'Průměrná cena za jednotku')
+	ORDER BY c.country, e.`year`);
